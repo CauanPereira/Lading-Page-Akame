d3b4d85f9972297db995b7fe19a18697054a426e
Cauan Firmo <cauangame1.0@gmail.com>
2025-06-19 17:56:48 -0300

    Atualizando Responsividade:3

--- js/carrosel.js
+++ js/carrosel.js
@@ -17,13 +17,31 @@ const swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-next",// Botão de voltar
   },
   breakpoints: {
+
+    260: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    290: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
     320: {
       slidesPerView: 1,
       spaceBetween: 20,
     },
-    768: {
-      slidesPerView: 2,
+
+    500: {
+      slidesPerView: 1,
       spaceBetween: 20,
+    },
+
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      
     },
   },
 });
--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
-const menuButton = document.getElementById('menu-button');
-const mobileMenu = document.getElementById('nav-mobile');
-const header = document.getElementById('header');
+const menuButton = document.getElementById("menu-button");
+const mobileMenu = document.getElementById("nav-mobile");
+const header = document.getElementById("header");
 
 // Adiciona o evento de clique ao botão do menu
-menuButton.addEventListener('click', () => {
-    // Alterna a classe 'open' no menu para mostrá-lo ou escondê-lo
-    mobileMenu.classList.toggle('open');
-    
-    // Alterna a classe 'menu-open' no header para animar o ícone
-    header.classList.toggle('menu-open');
+menuButton.addEventListener("click", () => {
+  // Alterna a classe 'open' no menu para mostrá-lo ou escondê-lo
+  mobileMenu.classList.toggle("open");
+
+  // Alterna a classe 'menu-open' no header para animar o ícone
+  header.classList.toggle("menu-open");
 });
